5b34557a1f68453e3a93a772ad11bca8cbbab195
Jean Victor <jeanvictor.fa@gmail.com>
2025-06-03 17:40:24 -0300

    relação many-to-one entre Bloco e Atividade

--- src/main/java/com/devsuperior/desafio/entities/Atividade.java
+++ src/main/java/com/devsuperior/desafio/entities/Atividade.java
@@ -2,7 +2,9 @@ package com.devsuperior.desafio.entities;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -29,6 +31,9 @@ public class Atividade {
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
+
+    @OneToMany(mappedBy = "atividade")
+    private List<Bloco> blocos = new ArrayList<>();
 
     public Atividade() {
     }
@@ -83,5 +88,9 @@ public class Atividade {
 
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
+    }
+
+    public List<Bloco> getBlocos() {
+        return blocos;
     }
 }
